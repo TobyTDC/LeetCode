a301f1af61787d07231fb943052385acfe0979c2
Dongchen Tang <tdc09114@gmail.com>
2017-12-21 11:34:57 -0500

    Create ADD Two Numbers.cc

---
+++ ADD Two Numbers.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(0);
+        ListNode *tail = head;
+        
+        int carry = 0;
+        while(l1 || l2 || carry){
+            int n = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            tail -> next = new ListNode(n % 10);
+            carry = n / 10;
+            tail = tail -> next;
+            l1 = l1?l1 -> next:NULL;
+            l2 = l2?l2 -> next:NULL;
+        }
+        return head -> next;
+    }
+};
