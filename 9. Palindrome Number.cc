582b34b6776398b01c23d364f825f095f0b88355
Dongchen Tang <tdc09114@gmail.com>
2018-06-01 14:24:01 -0400

    Create 9. Palindrome Number.cc

---
+++ 9. Palindrome Number.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        long tmp = abs(x);
+        long reverse = 0;
+        
+        while (tmp)
+        {
+            reverse = reverse * 10 + (tmp % 10);
+            tmp /= 10;
+        }
+        return reverse == x;
+    }
+};
